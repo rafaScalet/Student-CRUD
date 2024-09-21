9add0c381234f358455c35b1fd407ad9d390a4fb
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-21 18:56:54 -0300

    student repository

---
+++ src/main/java/com/student/student_backend/repositories/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.student.student_backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.student.student_backend.entities.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+
+}
