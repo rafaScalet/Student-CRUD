e8075aba4d24bb9b04d07964b7a57658a9b9e821
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-21 19:12:31 -0300

    add persistent values into table student

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student (name, period, active) VALUES ('nome 1', 'manha', true);
+INSERT INTO student (name, period, active) VALUES ('nome 2', 'tarde', true);
+INSERT INTO student (name, period, active) VALUES ('nome 3', 'noite', true);
