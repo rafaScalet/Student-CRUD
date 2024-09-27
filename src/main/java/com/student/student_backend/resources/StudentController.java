5a7dd8d96afa380b014b0eae1878b3b62c4ecf81
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-27 14:26:20 -0300

    add get student by id endpoint

--- src/main/java/com/student/student_backend/resources/StudentController.java
+++ src/main/java/com/student/student_backend/resources/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.student.student_backend.entities.Student;
 import com.student.student_backend.services.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -19,6 +20,11 @@ public class StudentController {
 	@GetMapping("students")
 	public List<Student> getStudents () {
 		return service.getStudents();
+	}
+
+	@GetMapping("students/{id}")
+	public Student getStudentById (@PathVariable int id) {
+		return service.getStudentById(id);
 	}
 
 }
--- src/main/java/com/student/student_backend/services/StudentService.java
+++ src/main/java/com/student/student_backend/services/StudentService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.student.student_backend.entities.Student;
 import com.student.student_backend.repositories.StudentRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class StudentService {
 
@@ -16,6 +18,10 @@ public class StudentService {
 
 	public List<Student> getStudents () {
 		return repository.findAll();
+	}
+
+	public Student getStudentById (int id) {
+		return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Student not found"));
 	}
 
 }
